c7cc441933d229021cf023292849977738ff211f
Enzonieto92 <enzonietoo92@gmail.com>
2026-04-28 21:20:51 -0300

    fix compuerta

--- escenas/contenedor_ux.gd
+++
@@ -1,6 +0,0 @@
-extends SubViewportContainer
-
-@onready var sub_viewport = $SubViewport
-
-func _ready() -> void:
-	grab_focus()
--- scripts/contenedor_ux.gd
+++ scripts/contenedor_ux.gd
@@ -1,10 +1,6 @@
 extends SubViewportContainer
 
+@onready var sub_viewport = $SubViewport
+
 func _ready() -> void:
 	grab_focus()
-
-func _input(event: InputEvent) -> void:
-	$SubViewport.push_input(event)
-
-func _unhandled_input(event: InputEvent) -> void:
-	$SubViewport.push_unhandled_input(event)
