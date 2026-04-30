5ceae6c04b04e10114cda5558afd7dc3b879e8c7
Enzonieto92 <enzonietoo92@gmail.com>
2026-04-29 22:02:22 -0300

    animaciones simultaneas, arbol de animaciones

--- scripts/animation_controller.gd
+++ scripts/animation_controller.gd
@@ -1,7 +1,9 @@
-extends AnimationPlayer
+extends Node
 
+@onready var anim_tree: AnimationTree = $"../AnimationTree"
+@onready var movimiento_sm: AnimationNodeStateMachinePlayback = anim_tree["parameters/movimiento/playback"]
+@onready var ataque_sm: AnimationNodeStateMachinePlayback = anim_tree["parameters/ataque/playback"]
 @onready var player: CharacterBody3D = $".."
-@onready var label_UI: Label = get_tree().get_first_node_in_group("label_animacion")
 
 var defendiendo: bool = false
 var esperando_soltar := false
@@ -9,30 +11,35 @@ var animaciones_arma = ["atacar", "atacar_horizontal"]
 var animacion_en_curso: bool = false
 
 func _ready():
-	play("idle")
-	animation_finished.connect(_on_animation_finished)
+	anim_tree.active = true
+	anim_tree["parameters/Add2/add_amount"] = 0.0
+	movimiento_sm.travel("idle")
 
 func resetear_estado() -> void:
 	animacion_en_curso = false
 	defendiendo = false
 	esperando_soltar = false
 	player.puede_correr = true
-	play("idle")
+	anim_tree["parameters/Add2/add_amount"] = 0.0
+	movimiento_sm.travel("idle")
 
-func _on_animation_finished(anim_name: String):
-	if anim_name in animaciones_arma or anim_name == "hit_bloqueado":
-		animacion_en_curso = false
-		player.puede_correr = true
-		if defendiendo:
-			var anim = get_animation("bloquear")
-			play("bloquear")
-			seek(anim.length, true)
-			pause()
-		elif Input.is_action_pressed("atacar") and is_instance_valid(player.arma):
-			play_random_animation()
+func _ataque_termino():
+	animacion_en_curso = false
+	player.puede_correr = true
+	anim_tree["parameters/Add2/add_amount"] = 0.0
+	if defendiendo:
+		ataque_sm.travel("bloquear")
+	elif Input.is_action_pressed("atacar") and is_instance_valid(player.arma):
+		play_random_animation()
 
-	if anim_name == "bloquear":
-		pause()
+func _actualizar_movimiento():
+	if player.moving:
+		if player.corriendo and player.puede_correr:
+			movimiento_sm.travel("correr")
+		else:
+			movimiento_sm.travel("caminar")
+	else:
+		movimiento_sm.travel("idle")
 
 func hold():
 	defendiendo = true
@@ -44,23 +51,30 @@ func on_block_hit():
 func on_anticipacion_completa():
 	if Input.is_action_pressed("atacar"):
 		esperando_soltar = true
-		pause()
+		anim_tree["parameters/TimeScale_ataque/scale"] = 0.0
 
 func _process(_delta: float) -> void:
-	label_UI.text = current_animation
+	# detectar fin de animación
+	if animacion_en_curso:
+		var progreso = ataque_sm.get_current_play_position()
+		var duracion = ataque_sm.get_current_length()
+		if duracion > 0 and progreso >= duracion - 0.05:
+			_ataque_termino()
+
+	# movimiento SIEMPRE se actualiza
+	_actualizar_movimiento()
 
 	if player.inventario_abierto:
-		if defendiendo:
-			defendiendo = false
+		defendiendo = false
 		esperando_soltar = false
-		if current_animation != "idle":
-			play("idle")
+		anim_tree["parameters/Add2/add_amount"] = 0.0
+		movimiento_sm.travel("idle")
 		return
 
 	if esperando_soltar:
 		if not Input.is_action_pressed("atacar"):
 			esperando_soltar = false
-			play(current_animation)
+			anim_tree["parameters/TimeScale_ataque/scale"] = 1.0
 		return
 
 	var bloqueando = Input.is_action_pressed("bloquear")
@@ -70,43 +84,34 @@ func _process(_delta: float) -> void:
 		if not defendiendo:
 			defendiendo = true
 			animacion_en_curso = false
-			play("bloquear")
+			anim_tree["parameters/Add2/add_amount"] = 1.0
+			ataque_sm.travel("bloquear")
 		return
 
 	if defendiendo:
 		defendiendo = false
-		play("idle")
+		anim_tree["parameters/TimeScale_ataque/scale"] = 1.0
+		anim_tree["parameters/Add2/add_amount"] = 0.0
 
 	if atacando_input:
 		if not animacion_en_curso:
 			play_random_animation()
 		return
 
-	if not animacion_en_curso:
-		if player.moving:
-			if player.corriendo and player.puede_correr:
-				if current_animation != "correr":
-					play("correr")
-			else:
-				if current_animation != "caminar":
-					play("caminar")
-		else:
-			if current_animation != "idle":
-				play("idle")
-
 func play_random_animation():
-	var disponibles = animaciones_arma.filter(func(a): return a != current_animation)
+	var current = ataque_sm.get_current_node()
+	var disponibles = animaciones_arma.filter(func(a): return a != current)
 	animacion_en_curso = true
 	player.puede_correr = false
-
+	anim_tree["parameters/Add2/add_amount"] = 1.0
 	if disponibles.is_empty():
-		play(animaciones_arma[0])
+		ataque_sm.travel(animaciones_arma[0])
 	else:
-		play(disponibles.pick_random())
+		ataque_sm.travel(disponibles.pick_random())
 
 func play_block_attack():
-	if current_animation == "hit_bloqueado":
+	if ataque_sm.get_current_node() == "hit_bloqueado":
 		return
-
 	animacion_en_curso = true
-	play("hit_bloqueado")
+	anim_tree["parameters/Add2/add_amount"] = 1.0
+	ataque_sm.travel("hit_bloqueado")
