879eb730a824cc60dba209563bb61acdc0898776
Enzonieto92 <enzonietoo92@gmail.com>
2026-05-05 23:19:08 -0300

    iluminación frontal y sonido de celda

--- scripts/celda.gd
+++ scripts/celda.gd
@@ -1,6 +1,7 @@
 extends MeshInstance3D
 
 @onready var pivote = $".."
+@onready var sonido_celda: AudioStreamPlayer3D = $sonido_celda
 
 var player_inside = false
 @export var abierta = true
@@ -18,12 +19,12 @@ func interactuar(_player):
 	var tween = create_tween()
 
 	if !abierta:
-		tween.tween_property(pivote, "rotation:y", deg_to_rad(65), 1.9)
+		tween.tween_property(pivote, "rotation:y", deg_to_rad(65), 4).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 		abierta = true
 	else:
-		tween.tween_property(pivote, "rotation:y", deg_to_rad(0), 1.9)
+		tween.tween_property(pivote, "rotation:y", deg_to_rad(0), 4).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 		abierta = false
-
+	sonido_celda.play()
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "player":
