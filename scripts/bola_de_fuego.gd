b9c085c971aac5c4930fa733349fea96c50b28c1
Enzonieto92 <enzonietoo92@gmail.com>
2026-05-13 00:54:48 -0300

    proyectil ya metiendo daño

--- scripts/bola_de_fuego.gd
+++ scripts/bola_de_fuego.gd
@@ -1,14 +1,28 @@
-extends Area3D  # o AnimatableBody3D
+extends Area3D
 
 @export var velocidad := 1.0
+@export var damage := Vector2(10, 15)
+
+
 var direccion := Vector3.FORWARD
 
+@onready var area_damage: Area3D = $area_de_damage      # nombre de tu nodo hijo
 
 func _ready():
-	rotation_degrees.y = 0
+	body_entered.connect(_on_hit)
+
+
+
 func _process(delta):
 	position += direccion * velocidad * delta
+
 func _on_hit(body):
-	if body.has_method("recibir_damage"):
-		body.recibir_damage(10)
+	explotar()
+
+func explotar():
+	for cuerpo in area_damage.get_overlapping_bodies():
+		if not cuerpo.has_method("recibir_damage"):
+			cuerpo = cuerpo.get_parent()
+		if cuerpo.has_method("recibir_damage"):
+			cuerpo.recibir_damage(damage)
 	queue_free()
