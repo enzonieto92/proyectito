983cb719048c5f8cfbb9d9966d8420f2c9b1d1e9
Enzonieto92 <enzonietoo92@gmail.com>
2026-06-03 21:41:20 -0300

    fix agachado

--- scripts/raycast_enemigo.gd
+++ scripts/raycast_enemigo.gd
@@ -4,13 +4,14 @@ extends RayCast3D
 var ya_golpeo: bool = false
 
 func _process(_delta: float) -> void:
-	look_at(player.position, Vector3.UP)
-	rotation.x = 0
-	rotation.z = 0
-	
+	look_at(player.position + Vector3(0,1.4,0), Vector3.UP)
+
 	if is_colliding() and not ya_golpeo:
+		set_enabled(false)
 		var obj = get_collider()
+		print (obj)
 		if obj.is_in_group("jugador"):
+			print ("hitting player")
 			ya_golpeo = true        # ← bloquear inmediatamente
-			set_enabled(false)
 			obj.recibir_damage(enemigo.damage, false)
+	
